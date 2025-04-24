27267fd8ec371effcebc22d35637d2cbdb226510
PhilipDet <philipdethlefsen251@gmail.com>
2025-04-24 13:26:34 +0200

    Icons

--- App/ts-modal-component/src/components/modal.tsx
+++ App/ts-modal-component/src/components/modal.tsx
@@ -1,4 +1,5 @@
 import { colors } from "../App";
+import { FaX } from "react-icons/fa6";
 
 interface ModalProps {
     isOpen: boolean;
@@ -9,7 +10,7 @@ interface ModalProps {
     children: React.ReactNode;
 }
 
-const sizes: any = {
+const sizes: Record<string, string> = {
     sm: "w-1/4 min-h-1/4",
     md: "w-1/2 min-h-1/2",
     lg: "w-3/4 min-h-3/4",
@@ -29,17 +30,28 @@ export default function Modal(props: ModalProps) {
             className="bg-black/40 w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center"
         >
             <div
-                className={`flex flex-col items-center ${colors[bgcolor]} ${sizes[boxSize]} rounded-lg p-4`}
+                className={`flex flex-col items-center ${
+                    fontColor === "text-white" ? "bg-black" : "bg-white"
+                } ${sizes[boxSize]} rounded-lg overflow-hidden`}
             >
-                <h1 className={`text-2xl font-bold ${fontColor}`}>Modal</h1>
-                <p className={`flex-1 ${fontColor}`}>{children}</p>
-
-                <button
-                    onClick={() => setIsOpen(false)}
-                    className="cursor-pointer py-2 px-4 bg-black text-white font-semibold rounded-lg shadow-md"
-                >
-                    Luk
-                </button>
+                <nav className={`w-full ${colors[bgcolor]}`}>
+                    <ul className="flex justify-between items-center w-full p-2">
+                        <li>
+                            <h1 className={`text-2xl font-bold ${fontColor}`}>
+                                Modal
+                            </h1>
+                        </li>
+                        <li>
+                            <button
+                                onClick={() => setIsOpen(false)}
+                                className={`cursor-pointer ${fontColor} flex justify-center items-center`}
+                            >
+                                <FaX className="text-2xl" />
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+                <p className={`flex-1 ${fontColor} p-3`}>{children}</p>
             </div>
         </div>
     ) : null;
